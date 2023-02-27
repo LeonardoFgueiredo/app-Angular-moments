4060b4bb5595816bbb5d9572a7ab34106e46de0f
Leonardo de Oliveira Figueiredo <leodbf@hotmail.com>
2023-02-27 14:26:45 -0300

    home

--- src/app/components/pages/home/home.component.ts
+++ src/app/components/pages/home/home.component.ts
@@ -1,35 +1,49 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { MomentService } from 'src/app/services/moment.service';
+
 import { Moment } from 'src/app/Moment';
+
 import { environment } from 'src/environments/environment';
-import { faSearchengin } from '@fortawesome/free-brands-svg-icons';
+
+import { faSearch } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   allMoments: Moment[] = [];
-  moments:    Moment[] = [];
+  moments: Moment[] = [];
   baseApiUrl = environment.baseApiUrl;
 
-  constructor(private momentService: MomentService){
+  searchTerm: string = '';
+  faSearch = faSearch;
 
+  constructor(private momentService: MomentService) {}
+
+  ngOnInit(): void {
+    this.momentService.getMoments().subscribe((items) => {
+      const data = items.data;
+
+      data.map((item) => {
+        item.created_at = new Date(item.created_at!).toLocaleDateString(
+          'pt-BR'
+        );
+      });
+
+      this.allMoments = items.data;
+      this.moments = items.data;
+    });
   }
 
-  ngOnInit(): void{
-    this.momentService.getMoments().subscribe((items)=>{
-      const data = items.data
-      data.map((item)=>{
-        item.created_at = new Date(item.created_at!).toLocaleDateString('pt-BR');
-      })
-      this.allMoments = data;
-      this.moments = data;
-    })
+  search(e: Event): void {
+    const target = e.target as HTMLInputElement;
+    const value = target.value;
+
+    this.moments = this.allMoments.filter((moment) =>
+      moment.title.toLowerCase().includes(value)
+    );
   }
-
-
-
 }
